9f0c38d7e1088641a8766e4305d8b612b77bf5ad
Jose Fletes <joseangelfletes@gmail.com>
2023-01-12 19:33:45 -0800

    Migrate database

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,11 @@
 class SessionsController < ApplicationController
+
+  def create #login
+
+  end
+
+  def destroy #logout
+
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+
+  def create #signup
+
+  end
+  
 end
