aae6cca16c765df3eb926dbcadf42e6be729795a
Jose Fletes <joseangelfletes@gmail.com>
2023-01-15 11:26:11 -0800

    Add signup form frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom'
 import Home from './Home'
 import NavBar from './NavBar'
+import Signup from './Signup';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    fetch("/me")
+      .then((r) => {
+        if (r.ok) {
+          r.json().then(user => setUser(user))
+        }
+      })
+  }, [])
+
+  if (!user) return <Home />
 
   return (
     <div className="App">
-        <NavBar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-        </Routes>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signup' element={<Signup onLogin={setUser} />} />
+      </Routes>
     </div>
   );
 }
---
+++ client/src/Signup.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+
+const Signup = ({ onLogin }) => {
+
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [passwordConfirmation, setPasswordConfirmation] = useState("")
+
+  const handleSubmit = (e) => {
+    
+    e.preventDefault()
+
+    fetch("/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username,
+        password,
+        password_confirmation: passwordConfirmation,
+      })
+    })
+    .then(r => r.json())
+    .then(onLogin)
+  }
+
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="username">Username:</label>
+      <input
+        type="text"
+        id="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <label htmlFor="password">Password:</label>
+      <input
+        type="password"
+        id="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <label htmlFor="password_confirmation">Confirm Password:</label>
+      <input
+        type="password"
+        id="password_confirmation"
+        value={passwordConfirmation}
+        onChange={(e) => setPasswordConfirmation(e.target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+
+    </div>
+  )
+}
+
+export default Signup
